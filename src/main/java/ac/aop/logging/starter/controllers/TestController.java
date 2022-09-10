0bd51f0bb6a10e7109ecde7643c827e9256b5c28
Andrea Cambieri <andrea.cambieri@gmail.com>
2022-09-10 12:11:43 +0200

    fix controller mapping

--- src/main/java/ac/aop/logging/starter/controllers/TestController.java
+++ src/main/java/ac/aop/logging/starter/controllers/TestController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/test")
+@RestController
+@RequestMapping("/test")
 public class TestController {
 
     HelloService helloService;
